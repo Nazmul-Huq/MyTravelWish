aeba5ea0712a81a75ceb1af322719d625889ddb5
Nazmul-Huq <hmnazmulhuq@gmail.com>
2023-05-17 20:52:32 +0200

    first setup

---
+++ app/src/main/java/com/nazmul/mytravelwish/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nazmul.mytravelwish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
